539d66a0ff816820a95bc2752008184f31848805
Esmail Ahbal <100035479+EsmailAHBAL@users.noreply.github.com>
2022-09-12 21:06:11 -0400

    second

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,8 @@
   <footer class="footer">
   <div class="content has-text-centered">
     <p>
-      <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      <strong>Esshop</strong> by <a href="https://esshop.netlify.app/">EsmailAHBAL</a>. The source code in GH
+      <a href="https://github.com/EsmailAHBAL">MIT</a>. https://github.com/EsmailAHBAL
     </p>
   </div>
 </footer>
